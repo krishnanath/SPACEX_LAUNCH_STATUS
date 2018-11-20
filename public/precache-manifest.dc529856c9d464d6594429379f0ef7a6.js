5fb0ff043d8ce2b2032a6f8320a36a5f78e19351
krishnanath <isabella0484@gmail.com>
2018-11-20 05:47:13 +0530

    Heroku Deployment

---
+++ public/precache-manifest.dc529856c9d464d6594429379f0ef7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62d81ebaa2e66edbfeca",
+    "url": "/static/js/main.62d81eba.chunk.js"
+  },
+  {
+    "revision": "638c271555c9281efe32",
+    "url": "/static/js/1.638c2715.chunk.js"
+  },
+  {
+    "revision": "62d81ebaa2e66edbfeca",
+    "url": "/static/css/main.70866198.chunk.css"
+  },
+  {
+    "revision": "639559091151cc5e6dec94b68241c27b",
+    "url": "/index.html"
+  }
+];
