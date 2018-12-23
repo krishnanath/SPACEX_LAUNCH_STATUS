a6c415538c065666294b70e54497318bf1dff79d
krishnanath <isabella0484@gmail.com>
2018-12-23 20:50:11 +0530

    frchng

--- schema.js
+++ schema.js
@@ -97,3 +97,26 @@ resolve(parent, args) {
 query: RootQuery
   });
 
+//addfordailyact
+
+rockets: {
+  type: new GraphQLList(RocketType),
+  resolve(parent, args) {
+    return axios
+      .get('https://api.spacexdata.com/v3/rockets')
+      .then(res => res.data);
+  }
+},
+rocket: {
+  type: RocketType,
+  args: {
+    id: { type: GraphQLInt }
+  },
+  resolve(parent, args) {
+    return axios
+      .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+      .then(res => res.data);
+  }
+}
+}
+});
